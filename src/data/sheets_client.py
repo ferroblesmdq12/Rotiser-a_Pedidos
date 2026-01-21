4c287583938c34b8535155a724e5ff72ef47e42c
ferroblesmdq12 <fernandomdq12@gmail.com>
2026-01-21 12:00:06 -0300

    debug de mail client

--- src/data/sheets_client.py
+++ src/data/sheets_client.py
@@ -24,3 +24,8 @@ def open_worksheet(spreadsheet_id: str, worksheet_name: str) -> gspread.Workshee
 def read_all_values(spreadsheet_id: str, worksheet_name: str) -> List[List[str]]:
     ws = open_worksheet(spreadsheet_id, worksheet_name)
     return ws.get_all_values()
+
+
+def debug_sa_email() -> str:
+    sa_info = dict(st.secrets["gcp_service_account"])
+    return str(sa_info.get("client_email", ""))
